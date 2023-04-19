4292247292a10d5b5447543d981a229ceb6a2ada
AmanYadav2369 <130037525+AmanYadav2369@users.noreply.github.com>
2023-04-19 12:11:17 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,19 @@
+/* wajp to print fibonacci series.  */
+
+
+class fibonacci
+{
+public static void main(String args[])
+{
+int a=0, b=1;
+System.out.print(a+" "+b);
+int c;
+for(int i=1; i<=15; i++){
+c=a+b;
+System.out.print(" "+c);
+a=b;
+b=c;
+}
+
+}
+}
